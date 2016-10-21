2ee2f7a8811bfb09e6307daba82a85ffa35bd692
jaga0228_mac <jaga0228@gmail.com>
2016-10-21 19:12:34 +0900

    fix delayTime & delete duplicated codes

--- src/com/linq/Main.java
+++ src/com/linq/Main.java
@@ -23,121 +23,26 @@ public class Main
 	private static final int IRDIST3 = 5;
 	private static final int IRDIST4 = 6;
 	private static final int SRDIST = 7;
-	
-	private static byte convRSSignal(int data) {
-	    if(data >= 64) {
-	        return (byte)((-2 * data) -1);
-	    }else{
-	        return (byte)((-2 * data) + 255);
-	    }
-	}
+
 	
 	public void run() {
-		int sendData = 1;
 		//ボーレートとバッファサイズの設定
 		RS485.hsEnable(9600, 0);
 		//データ格納用の配列
 		byte send[] = new byte[7];
 		//データ受信用の配列
 		byte get[] = new byte[7];
-		byte get2[] = new byte[1];
 		send[0] = 0;
 		
-		boolean flag = false;
 		while(true) {
-			
 			RS485.hsWrite(send, 0, 1);
 			RS485.hsRead(get, 0, 7);
-//			RS485.hsRead(get, 1, 1);
 
 			LCD.clear();
 			for(int i = 0; i < 7; i++) {
 				LCD.drawInt(get[i], 0, i);
 			}
-			Delay.msDelay(5);
-//			Delay.msDelay(50);
-			
-			if(flag == true) break;
-		}
-		
-		while(true) {
-			LCD.clear();
-			//===============================
-			sendData = TEMP1;
-			send[0] = convRSSignal(sendData);
-			RS485.hsWrite(send, 0, 1);
-
-			get[0] = -1;
-			while(get[0] == -1) {
-				RS485.hsRead(get, 0, 1);
-			}
-
-			LCD.drawInt(sendData, 0, 0);
-			LCD.drawInt(get[0], 4, 0);
-
-			Delay.msDelay(50);
-			//===============================
-			sendData = TEMP2;
-			send[0] = convRSSignal(sendData);
-			RS485.hsWrite(send, 0, 1);
-
-			get[0] = -1;
-			while(get[0] == -1) {
-				RS485.hsRead(get, 0, 1);
-			}
-			LCD.drawInt(sendData, 0, 1);
-			LCD.drawInt(get[0], 4, 1);
-			Delay.msDelay(50);
-			//===============================
-			sendData = IRDIST1;
-			send[0] = convRSSignal(sendData);
-			RS485.hsWrite(send, 0, 1);
-
-			get[0] = -1;
-			while(get[0] == -1) {
-				RS485.hsRead(get, 0, 1);
-			}
-			LCD.drawInt(sendData, 0, 2);
-			LCD.drawInt(get[0], 4, 2);
-			Delay.msDelay(50);
-			//===============================
-			sendData = IRDIST2;
-			send[0] = convRSSignal(sendData);
-			RS485.hsWrite(send, 0, 1);
-
-			get[0] = -1;
-			while(get[0] == -1) {
-				RS485.hsRead(get, 0, 1);
-			}
-			LCD.drawInt(sendData, 0, 3);
-			LCD.drawInt(get[0], 4, 3);
-			Delay.msDelay(50);
-			//===============================
-			sendData = IRDIST3;
-			send[0] = convRSSignal(sendData);
-			RS485.hsWrite(send, 0, 1);
-
-			get[0] = -1;
-			while(get[0] == -1) {
-				RS485.hsRead(get, 0, 1);
-			}
-			LCD.drawInt(sendData, 0, 4);
-			LCD.drawInt(get[0], 4, 4);
-			Delay.msDelay(50);
-			//===============================
-			sendData = IRDIST4;
-			send[0] = convRSSignal(sendData);
-			RS485.hsWrite(send, 0, 1);
-
-			get[0] = -1;
-			while(get[0] == -1) {
-				RS485.hsRead(get, 0, 1);
-			}
-			LCD.drawInt(sendData, 0, 5);
-			LCD.drawInt(get[0], 4, 5);
-			Delay.msDelay(50);
-			//===============================
-			Delay.msDelay(500);
+			Delay.msDelay(10);
 		}
 	}
 	
