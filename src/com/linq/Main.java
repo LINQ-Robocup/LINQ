b341c90121ff13176de8db8a4e53dc5a79a2b83c
jaga0228_mac <jaga0228@gmail.com>
2016-10-26 18:40:55 +0900

    remove comment

--- src/com/linq/Main.java
+++ src/com/linq/Main.java
@@ -16,17 +16,9 @@ import lejos.util.Stopwatch;
 
 public class Main
 {
-	private static final int TEMP1 = 1;
-	private static final int TEMP2 = 2;
-	private static final int IRDIST1 = 3;
-	private static final int IRDIST2 = 4;
-	private static final int IRDIST3 = 5;
-	private static final int IRDIST4 = 6;
-	private static final int SRDIST = 7;	
 
 	public static void main(String args[]) {
 		
-		//ÉÇÅ[É^Å[ÇÃêÈåæ
 //		LQMotor2 leftMotor = new LQMotor2(MotorPort.A);
 //		LQMotor2 rightMotor = new LQMotor2(MotorPort.C);
 //		LQMover mover = new LQMover(MotorPort.A, MotorPort.B);
