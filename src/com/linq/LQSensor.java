1a9938ab7ce1f7081162b5142738688979df247d
jaga0228_mac <jaga0228@gmail.com>
2016-11-18 20:17:04 +0900

    Update: control servo from RS485 signal

--- src/com/linq/LQSensor.java
+++ src/com/linq/LQSensor.java
@@ -1,6 +1,8 @@
 package com.linq;
 
 import lejos.nxt.LCD;
+import lejos.nxt.SensorPort;
+import lejos.nxt.addon.CruizcoreGyro;
 import lejos.nxt.comm.RS485;
 import lejos.util.Delay;
 
@@ -16,12 +18,16 @@ public class LQSensor {
 	public int temp2;
 	public int sr;
 	
+	public CruizcoreGyro gyro;
+	
 	public LQSensor() {
 		RS485.hsEnable(9600, 0);
 		send[0] = 0;
+		gyro = new CruizcoreGyro(SensorPort.S3);
 	}
 	
 	public void readAllSensors() {
+		send[0] = 0;
 		RS485.hsWrite(send, 0, 1);
 		RS485.hsRead(get, 0, 7);
 		irDist1	= get[0];
@@ -48,6 +54,24 @@ public class LQSensor {
 		LCD.drawString("temp2", 0, 5);		LCD.drawInt(get[5], showValueOffset, 5);
 		LCD.drawString("sr", 0, 6); 		LCD.drawInt(get[6], showValueOffset, 6);
 
+		Delay.msDelay(10);
+	}
+	
+	public int getGyroValue() {
+		return gyro.getAngle();
+	}
+	
+	public void resetGyroValue() {
+		gyro.reset();
+	}
+	
+	public void rotateServo(int angle) {
+		if(angle == 0) {
+			send[0] = 8;
+		}else{
+			send[0] = 9;
+		}
+		RS485.hsWrite(send, 0, 1);
 		Delay.msDelay(10);
 	}
 }
--- src/com/linq/Main.java
+++ src/com/linq/Main.java
@@ -1,19 +1,27 @@
 package com.linq;
 
+import lejos.nxt.LCD;
 import lejos.nxt.MotorPort;
+import lejos.util.Delay;
 
 public class Main
 {
 
 	public static void main(String args[]) {
 		
-//		LQMotor2 leftMotor = new LQMotor2(MotorPort.A);
-//		LQMotor2 rightMotor = new LQMotor2(MotorPort.C);
+		LQMotor2 leftMotor = new LQMotor2(MotorPort.A);
+		LQMotor2 rightMotor = new LQMotor2(MotorPort.C);
 //		LQMover mover = new LQMover(MotorPort.A, MotorPort.B);
 		LQSensor sensor = new LQSensor();
-
+		
+		rightMotor.setPower(100);
+		rightMotor.forward();
+		
 		while(true) {
-			sensor.showAllSensors();
+			sensor.rotateServo(0);
+			Delay.msDelay(500);
+			sensor.rotateServo(180);
+			Delay.msDelay(500);
 		}
 	}
-}
+}	
