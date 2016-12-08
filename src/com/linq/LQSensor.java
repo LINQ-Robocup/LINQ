e778008defbd3e20a21fa2e457cd7d48e5c2738c
jaga0228_mac <jaga0228@gmail.com>
2016-12-08 19:41:58 +0900

    Update sensor class

--- src/com/linq/LQSensor.java
+++ src/com/linq/LQSensor.java
@@ -1,49 +1,113 @@
 package com.linq;
 
 import lejos.nxt.LCD;
+import lejos.nxt.MotorPort;
+import lejos.nxt.NXTMotor;
 import lejos.nxt.SensorPort;
 import lejos.nxt.addon.CruizcoreGyro;
 import lejos.nxt.comm.RS485;
 import lejos.util.Delay;
 
 public class LQSensor {
-	private byte send[] = new byte[1];
+	private byte send[] = new byte[2];
 	private byte get[] = new byte[7];
 	
-	public int irDist1;
-	public int irDist2;
-	public int irDist3;
-	public int irDist4;
-	public int temp1;
-	public int temp2;
-	public int sr;
+	public final int IRDIST_RIGHT	= 0;
+	public final int IRDIST2	= 1;
+	public final int IRDIST3	= 2;
+	public final int IRDIST_LEFT	= 3;
+	public final int TEMP1		= 4;
+	public final int TEMP2		= 5;
+	public final int SRDIST		= 6;
+	
+	private int irDist1Value;
+	private int irDist2Value;
+	private int irDist3Value;
+	private int irDist4Value;
+	private int temp1Value;
+	private int temp2Value;
+	private int srValue;
 	
 	public CruizcoreGyro gyro;
 	
 	public LQSensor() {
 		RS485.hsEnable(9600, 0);
-		send[0] = 0;
+		send[0] =1;
 		gyro = new CruizcoreGyro(SensorPort.S3);
+
+		//servo
+		NXTMotor servo = new NXTMotor(MotorPort.C);
 	}
-	
 	public void readAllSensors() {
-		send[0] = 0;
+		send[0] = 1;
 		RS485.hsWrite(send, 0, 1);
 		RS485.hsRead(get, 0, 7);
-		irDist1	= get[0];
-		irDist2	= get[1];
-		irDist3	= get[2];
-		irDist4	= get[3];
-		temp1	= get[4];
-		temp2	= get[5];
-		sr		= get[6];
+		irDist1Value	= get[0];
+		irDist2Value	= get[1];
+		irDist3Value	= get[2];
+		irDist4Value	= get[3];
+		temp1Value		= get[4];
+		temp2Value		= get[5];
+		srValue			= get[6];
 		Delay.msDelay(10);
 	}
+	public int getSensorValue(int sensor) {
+		readAllSensors();
+		switch (sensor) {
+		case IRDIST_RIGHT:
+			return irDist1Value;
+		case IRDIST2:
+			return irDist2Value;
+		case IRDIST3:
+			return irDist3Value;
+		case IRDIST_LEFT:
+			return irDist4Value;
+		default:
+			break;
+		}
+		
+		return -1;
+	}
+//	public int getSensorValue(int sensor) {
+//		int value = 0;
+//		switch (sensor) {
+//		case 0:
+//			send[0] = 1;
+//			RS485.hsWrite(send, 0, 1);
+//			RS485.hsRead(get, 0, 1);
+//			irDist1Value	= get[0];
+//			Delay.msDelay(10);
+//			break;
+//		case 1:
+//			
+//			break;
+//		case 2:
+//			
+//			break;
+//		case 3:
+//			
+//			break;
+//		case 4:
+//			
+//			break;
+//		case 5:
+//			
+//			break;
+//
+//
+//		default:
+//			break;
+//		}
+//		
+//		return value;
+//	}
 	
 	public void showAllSensors() {
 		int showValueOffset = 9;
-		RS485.hsWrite(send, 0, 1);
-		RS485.hsRead(get, 0, 7);
+//		send[0] = 1;
+//		RS485.hsWrite(send, 0, 1);
+//		RS485.hsRead(get, 0, 7);
+		readAllSensors();
 		
 		LCD.clear();
 		LCD.drawString("irDist1", 0, 0);	LCD.drawInt(get[0], showValueOffset, 0);
--- src/com/linq/Main.java
+++ src/com/linq/Main.java
@@ -1,8 +1,11 @@
 package com.linq;
 
+import lejos.nxt.Button;
 import lejos.nxt.LCD;
 import lejos.nxt.MotorPort;
 import lejos.util.Delay;
+
+import java.lang.Math;
 
 public class Main
 {
@@ -10,18 +13,41 @@ public class Main
 	public static void main(String args[]) {
 		
 		LQMotor2 leftMotor = new LQMotor2(MotorPort.A);
-		LQMotor2 rightMotor = new LQMotor2(MotorPort.C);
-//		LQMover mover = new LQMover(MotorPort.A, MotorPort.B);
+		LQMotor2 rightMotor = new LQMotor2(MotorPort.B);
+		LQMover mover = new LQMover(MotorPort.A, MotorPort.B);
 		LQSensor sensor = new LQSensor();
 		
-		rightMotor.setPower(100);
-		rightMotor.forward();
-		
-		while(true) {
-			sensor.rotateServo(0);
-			Delay.msDelay(500);
-			sensor.rotateServo(180);
-			Delay.msDelay(500);
+		leftMotor.setPower(40);
+		rightMotor.setPower(40);
+		Button
+		while () {
+			
 		}
+		int leftOffset = sensor.getSensorValue(sensor.IRDIST_LEFT);
+		int rightOffset = sensor.getSensorValue(sensor.IRDIST_RIGHT);
+		LCD.drawInt(leftOffset, 0, 0);
+		LCD.drawInt(rightOffset, 0, 1);
+		if(leftOffset > rightOffset) {
+			//go right
+			while (sensor.getSensorValue(sensor.IRDIST_LEFT) > rightOffset) {
+				leftMotor.forward();
+				rightMotor.stop();
+			}
+		}else {
+			//go left
+			while (sensor.getSensorValue(sensor.IRDIST_RIGHT) > leftOffset) {
+				leftMotor.stop();
+				rightMotor.forward();
+			}
+		}
+		leftMotor.stop();
+		rightMotor.stop();
+		
+//		while (true) {
+//			LCD.clear();
+//			LCD.drawInt(sensor.getSensorValue(sensor.IRDIST1), 0, 0);
+//			LCD.drawInt(sensor.getSensorValue(sensor.IRDIST4), 10, 0);
+//			Delay.msDelay(10);
+//		}
 	}
 }	
